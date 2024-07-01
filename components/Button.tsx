f7d63b7f6c1498dd47c18352eb72da7e14dd5917
Xamarsia <67323429+Xamarsia@users.noreply.github.com>
2024-06-30 22:57:55 -0400

    [SPSP-96] Add button component

---
+++ components/Button.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import styles from '@/app/styles/components/button.module.css'
+import { useState } from 'react'
+import Image from 'next/image'
+
+
+type ButtonProps = {
+    type: 'button' | 'submit';
+    text: string | undefined;
+    style: 'primary-button' | 'secondary-button' | 'transparent-button' | 'delete-button' | 'delete-transparent-button';
+    size?: 'extra-small' | 'small' | 'base' | 'large' | 'extra-large';
+    rounded?: 'rounded' | 'rounded-md' | 'rounded-lg' | 'rounded-xl';
+    fill?: 'content' | 'parent';
+    disabled?: boolean;
+    icon?: string;
+    hoveredIcon?: string;
+    iconSide?: 'left' | 'right';
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+
+export default function Button({ icon, text, style, type = 'button', size = 'base', rounded = 'rounded-md', fill = "content", disabled = false, iconSide = 'left', hoveredIcon = icon, onClick }: ButtonProps) {
+    const [isHovering, setIsHovered] = useState(false);
+    const onMouseEnter = () => setIsHovered(true);
+    const onMouseLeave = () => setIsHovered(false);
+
+    var iconSize: string = size + `-icon`;
+    var iconGap: string = size + `-gap`;
+    return (
+        <button type={type}
+            className={`
+                ${styles['base-button']} 
+                ${styles[style]} 
+                ${styles[size]}
+                ${rounded}
+                ${(text && icon) ? styles[iconGap] : ''}
+                ${fill == 'parent' ? 'w-full' : ''}
+                ${icon ? 'flex justify-between' : ''}
+            `}
+            disabled={disabled}
+            onClick={onClick}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+        >
+
+            {icon && hoveredIcon &&
+                <div className={`${iconSide == 'right' ? 'order-last' : 'order-first'}`}>
+                    <div className={`${styles[iconSize]}`}>
+                        <Image src={isHovering ? hoveredIcon : icon} alt="icon" />
+                    </div>
+                </div>
+            }
+            <h1 className={`items-center`}>{text}</h1>
+
+        </button>
+    )
+}
