b4387e87c60e103a9e065c533f3e49ff47c14151
Xamarsia <67323429+Xamarsia@users.noreply.github.com>
2024-07-11 22:26:40 -0400

    [SPSP-122] Add not found component

---
+++ components/common/NotFound.tsx
@@ -0,0 +1,21 @@
+import blueExclamationCircle from '@/public/exclamation-circle/blue-exclamation-circle.svg';
+
+import Image from 'next/image';
+import textStyles from '@/app/styles/components/text.module.css';
+
+type Props = {
+    alertTitle: string,
+    alertBody?: string,
+}
+
+export default function NotFound({ alertTitle, alertBody }: Props) {
+    return (
+        <div className="flex flex-col items-center mx-auto px-6 py-12 gap-2">
+            <div className="rounded-full bg-blue-50 p-3">
+                <Image src={blueExclamationCircle} alt="icon" />
+            </div>
+            <h1 className={`${textStyles["title-large"]}`}>{alertTitle}</h1>
+            <p className={`${textStyles["secondary-info"]}`}>{alertBody}</p>
+        </div>
+    );
+};
