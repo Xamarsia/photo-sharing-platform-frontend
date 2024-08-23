73387ef3fbe5cbcbb450a0225784e89ade12db16
xamarsia <xamarsia@gmail.com>
2024-08-22 23:23:42 -0400

    Add edit post fetch

--- actions/post-actions.tsx
+++ actions/post-actions.tsx
@@ -11,3 +11,22 @@ export async function createPost(data: FormData): Promise<PostDTO | undefined> {
     const post: PostDTO = await res.json();
     return post;
 }
+
+export async function getDetailedPost(postId: number): Promise<DetailedPostDTO | undefined> {
+    const res: Response = await authFetch(`/post/${postId}/detailed`, { method: 'GET' });
+    if (!res.ok) {
+        return undefined;
+    }
+    const detailedPost: DetailedPostDTO = await res.json();
+    return detailedPost;
+}
+
+
+export async function getPost(postId: number): Promise<PostDTO | undefined> {
+    const res: Response = await authFetch(`/post/${postId}`, { method: 'GET' });
+    if (!res.ok) {
+        return undefined;
+    }
+    const post: PostDTO = await res.json();
+    return post;
+}
--- app/post/[postId]/page.tsx
+++ app/post/[postId]/page.tsx
@@ -1,24 +1,29 @@
 import 'server-only';
 
 import { getDictionary } from '@/lib/localization';
-import { getDetailedPost } from '@/lib/post-controller';
 
 import Post from '@/components/post/Post';
+import NotFound from '@/components/common/NotFound';
+
+import { getDetailedPost } from '@/actions/post-actions';
 
 
 type PageProps = {
-    postId: string,
+    postId: number,
 }
 
 
 export default async function EditPostPage({ params }: { params: PageProps }) {
     const dict = await getDictionary('en');
-    const postrData: DetailedPostDTO = await getDetailedPost(params.postId);
 
+    const detailedPost: DetailedPostDTO | undefined = await getDetailedPost(params.postId);
 
     return (
         <div className='flex flex-grow flex-shrink justify-center items-center'>
-            <Post detailedPost={postrData} local={dict} />
+            {detailedPost
+                ? <Post detailedPost={detailedPost} local={dict} />
+                : <NotFound alertTitle={dict.postNotFound} alertBody={dict.postDoesNotExist} />
+            }
         </div>
     );
 }
