2b94bfb7f89f0bc57167f05c7445eaa7ba05d1e2
xamarsia <xamarsia@gmail.com>
2024-09-01 02:33:51 +0000

    fix: Select current sidebar element

--- components/common/SidebarItem.tsx
+++ components/common/SidebarItem.tsx
@@ -1,25 +1,21 @@
 "use client";
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { useState } from 'react';
+
 
 type Props = {
     text: string,
     href: string,
+    selected?: boolean,
 }
 
 
-export default function SidebarItem({ text, href }: Props) {
-    const pathname = usePathname()
-    const [isCurrentItemSelected] = useState<boolean>(href == pathname);
-
-
+export default function SidebarItem({ text, href, selected }: Props) {
     return (
         <Link href={href}
             className={`flex items-center px-3 rounded-lg h-10 w-full min-w-56
                 text-sm font-normal text-gray-500 hover:text-gray-700 hover:bg-gray-100
-                ${isCurrentItemSelected ? 'bg-gray-100 text-gray-800' : 'bg-white'}
+                ${selected ? 'bg-gray-100 text-gray-800' : 'bg-white'}
             `}
         >
             {text}
