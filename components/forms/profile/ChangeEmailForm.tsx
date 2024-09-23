3ca2a39d055b33d042072cba9650e2be1c0ce540
xamarsia <xamarsia@gmail.com>
2024-09-23 02:59:04 +0000

    Add change email form validation

--- components/forms/profile/ChangeEmailForm.tsx
+++ components/forms/profile/ChangeEmailForm.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-
+import Modal from '@/components/common/Modal';
 import Input from '@/components/common/Input';
 import TextButton from '@/components/buttons/TextButton';
 import FormFieldError from '@/components/common/FormFieldError';
+import VerifyEmailForm from '@/components/forms/profile/VerifyEmailForm';
 
 import { ChangeEvent, FormEvent, useState } from "react";
 import { getValidationErrors } from '@/lib/zod/validation';
@@ -12,19 +13,38 @@ import { updateEmailSchema } from '@/lib/zod/schemas/profile/changeEmail';
 
 type Props = {
     local: any,
-    onSubmit?: (event: FormEvent<HTMLFormElement>) => void,
+    oldEmail: string
 }
 
-export default function ChangeEmailForm({ local, onSubmit }: Props) {
+export default function ChangeEmailForm({ local, oldEmail }: Props) {
     const [isFormChanged, setIsFormChanged] = useState<boolean>(false);
     const [errors, setErrors] = useState<Map<string | number, string>>(new Map());
-    const [email, setEmail] = useState("");
+    const [email, setEmail] = useState<string>(oldEmail);
+    const [showModal, setShowModal] = useState(false);
 
     async function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        if (onSubmit) {
-            onSubmit(event);
+
+        if (!isFormChanged || !email) {
+            return;
         }
+
+        if (email == oldEmail) {
+            setIsFormChanged(false);
+            return;
+        }
+
+        const response = updateEmailSchema.safeParse({
+            email: email,
+        });
+
+        const errorsMap: Map<string | number, string> = getValidationErrors(response);
+        if (errorsMap.size != 0) {
+            setErrors(errorsMap);
+            return;
+        }
+
+        setShowModal(true);
     }
 
     async function onEmailChangeHendler(event: ChangeEvent<HTMLInputElement>) {
@@ -39,26 +59,33 @@ export default function ChangeEmailForm({ local, onSubmit }: Props) {
     }
 
     return (
-        <form onSubmit={handleSubmit}
-            onChange={(e) => { setIsFormChanged(true) }}
-            className={`text-left flex flex-col gap-y-3 sm:gap-y-6`}>
-            <div>
-                <Input
-                    type="text"
-                    name="email"
-                    value={email}
-                    title={local.email}
-                    onChange={(e) => onEmailChangeHendler(e)}
-                    required
+        <div>
+            <form
+                onSubmit={handleSubmit}
+                onChange={(e) => { setIsFormChanged(true) }}
+                className={`text-left flex flex-col gap-y-3 sm:gap-y-6`}>
+                <div>
+                    <Input
+                        type="text"
+                        name="email"
+                        value={email}
+                        title={local.email}
+                        onChange={(e) => onEmailChangeHendler(e)}
+                        required
+                    />
+                    <FormFieldError text={errors.get("email")} />
+                </div>
+                <TextButton
+                    type="submit"
+                    text={local.update}
+                    fill="content"
+                    disabled={!isFormChanged || errors.size != 0}
                 />
-                <FormFieldError text={errors.get("email")} />
-            </div>
-            <TextButton
-                type="submit"
-                text={local.update}
-                fill="content"
-                disabled={!isFormChanged || errors.size != 0}
-            />
-        </form>
+            </form>
+
+            <Modal title={local.verifyNewEmailAddress} onCloseClicked={() => { setShowModal(false); }} opened={showModal}>
+                <VerifyEmailForm local={local} newEmail={email} />
+            </Modal>
+        </div>
     )
 }
---
+++ components/forms/profile/VerifyEmailForm.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import styles from '@/app/styles/text/text.module.css';
+
+import { useRouter } from 'next/navigation';
+import { FormEvent, useState } from "react";
+
+import Input from '@/components/common/Input';
+import TextButton from '@/components/buttons/TextButton';
+// import { reauthenticate, updateUserEmail, verifyEmail } from '@/lib/firebase/auth';
+
+type Props = {
+    local: any,
+    newEmail: string,
+}
+
+
+export default function VerifyEmailForm({ local, newEmail }: Props) {
+    const [password, setPassword] = useState("password");
+    const [formIsValid, setFormIsValid] = useState(true);
+    const [confirmPressed, setConfirmPressed] = useState(false);
+
+    const router = useRouter();
+
+    async function handleReAuthentication(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        // TODO realise email update
+        // const isAuthorized = await reauthenticate(password);
+        // await verifyEmail(newEmail);
+        // await updateUserEmail(newEmail);
+
+        setConfirmPressed(true);
+    }
+
+
+    return (
+        <form onSubmit={handleReAuthentication}
+            onChange={(e) => setFormIsValid(e.currentTarget.checkValidity())}
+            className={`flex flex-col justify-between h-[464px]`}>
+            <div className={`flex flex-col gap-y-3`}>
+                <p className={`${styles['base-text']}`}>{confirmPressed ? local.resetEmailMessageSended : local.resetEmailMessage} </p>
+
+                {!confirmPressed && <Input
+                    type="password"
+                    name="password"
+                    title={local.password}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />}
+            </div>
+
+            {!confirmPressed && <TextButton
+                type="submit"
+                text={local.confirm}
+                fill="content"
+                disabled={!formIsValid}
+            />}
+        </form>
+    )
+}
