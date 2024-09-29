6e68c17270ca36f595ab2314b087873d19e481fa
xamarsia <xamarsia@gmail.com>
2024-09-29 05:29:17 +0000

    [fix] Change posts preview grid layout

--- components/post/PostsPreviewGrid.tsx
+++ components/post/PostsPreviewGrid.tsx
@@ -29,9 +29,9 @@ export default function PostsPreview({ posts }: PostsProps) {
 
 
     return (
-        <ul className='flex flex-wrap justify-start gap-0.5 md:gap-1'>
+        <ul className='flex flex-grow size-full flex-wrap content-start gap-0.5 md:gap-1'>
             {posts.map(post => (
-                <li key={post.id} className='flex-1 basis-3/12'>
+                <li key={post.id} className='flex flex-auto basis-3/12'>
                     <PostPreview post={post} />
                 </li>
             ))}
